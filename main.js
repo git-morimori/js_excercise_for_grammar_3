223a85f1f15a075d83cca7cfa8491e7c25d66231
haruka-g <mortndpgj@gmail.com>
2019-09-30 00:27:37 +0900

    modified commit main.js

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ while (num < 100) {
 //       - answerとinputの値が同じだったら「正解です！」とalertで表示する
 //     - 正解が出るまでpromptで問題文を出し続ける
 //     - 正解が出たらWhileループを抜ける
-let question = 'パンはパンでも食べられないパンは？';
-let answer = 'フライパン';
+const question = 'パンはパンでも食べられないパンは？';
+const answer = 'フライパン';
 let input;
 while (answer !== input) {
     input = prompt(question);
